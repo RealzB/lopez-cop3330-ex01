8ad6f9b53dd8a52787664a8d3f0e9faf7f0d6209
Brandon <nickelboarder123.bl@gmail.com>
2021-09-13 22:18:46 -0400

    code in wrong place

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,22 @@
 package org.example;
 
-/**
- * Hello world!
- *
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Brandon Lopez
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("What is your name?");
+
+        String name = myObj.nextLine();
+        System.out.println(" Hello, " + name + ", nice to meet you!");
+
     }
 }
